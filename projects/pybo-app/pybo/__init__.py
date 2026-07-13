fd8668f653d51b369c52710b8c933df7c0c81519
kim won il <gygs10990@gmail.com>
2026-07-13 20:13:54 +0900

    커밋

--- projects/pybo-app/pybo/__init__.py
+++ projects/pybo-app/pybo/__init__.py
@@ -24,24 +24,22 @@ def create_app():
     with app.app_context():
         db.create_all()
         if models.Question.query.count() == 0:
-            q1 = models.Question(
-                subject='안녕하세요',
-                content='가입 인사드립니다 ^^',
-                create_date=datetime.now(),
-            )
-            q2 = models.Question(
-                subject='두 번째 글',
-                content='내용입니다',
-                create_date=datetime.now(),
-            )
-            q3 = models.Question(
-                subject='나는 김원일 입니다.',
-                content='나는 힘듭니다ㅎㅎ',
-                create_date=datetime.now(),
-            )
-            db.session.add(q1)
-            db.session.add(q2)
-            db.session.add(q3)
+            q1 = models.Question()
+            q1.subject = '안녕하세요'
+            q1.content = '가입 인사드립니다 ^^'
+            q1.create_date = datetime.now()
+
+            q2 = models.Question()
+            q2.subject = '두 번째 글'
+            q2.content = '내용입니다'
+            q2.create_date = datetime.now()
+
+            q3 = models.Question()
+            q3.subject = '나는 김원일 입니다.'
+            q3.content = '나는 힘듭니다ㅎㅎ'
+            q3.create_date = datetime.now()
+
+            db.session.add_all([q1, q2, q3])
             db.session.commit()
 
     return app
